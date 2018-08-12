1dfae1f6699520426e028f498d4fa0cd8c7736af
Andrew Gregory <andrew@claritysquared.com>
2018-08-11 22:10:03 -0400

    removed login form that is pop-up

--- wp-content/themes/coincademy_reborn/header.php
+++ wp-content/themes/coincademy_reborn/header.php
@@ -20,18 +20,7 @@
 
 	</head>
 	<body <?php echo body_class(); ?>>
-	<!-- REMOVED AS HAVE NEW PAGE FOR LOGIN INSTEAD OF THIS...
 
-    	<div class="auth-holder" id="auth-holder-id">
-            <div class="background-layer" id="background-layer"></div>
-            <div class="auth-form-holder active" id="login-holder">
-            	<?php echo do_shortcode('[lifterlms_login layout="columns" redirect="'.'esc_url( $redirect )'.'"]'); ?>
-            </div>
-            <div class="auth-form-holder" id="registration-holder">
-            	<?php get_template_part('auth', 'register'); ?>
-            </div>
-		</div> -->
-	
         <nav class="top-navigation <?php if (is_course()) { echo "white-nav"; echo " ".strtolower(wp_get_object_terms($post->ID, 'course_cat')[0]->name);} ?>" id="top-nav" role="navigation">
         	<div class="max-width-menu vmid">
             	<div class="pure-u-1 pure-u-lg-1-5 pure-u-md-1-5">
