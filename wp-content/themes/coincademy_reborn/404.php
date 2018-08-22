e4dfc83ed8cb12a09f5a9e6aa1a9bd9a7c3d286a
Andrew Gregory <andrew@claritysquared.com>
2018-08-22 08:21:36 -0400

    add basic 404 page

--- wp-content/themes/coincademy_reborn/404.php
+++ wp-content/themes/coincademy_reborn/404.php
@@ -2,14 +2,14 @@
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="section-first-no-header max-width">
 
 			<!-- article -->
 			<article id="post-404">
 
 				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
 				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
+					<a style="text-decoration: underline;" href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
 				</h2>
 
 			</article>
