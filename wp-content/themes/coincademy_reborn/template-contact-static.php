01cbc923463921c9f75d69c5ef2892d29c0e7f63
Alex Paden <ajp1016@sru.edu>
2018-09-05 20:21:19 -0400

    Content Update

--- wp-content/themes/coincademy_reborn/template-contact-static.php
+++ wp-content/themes/coincademy_reborn/template-contact-static.php
@@ -9,9 +9,9 @@
                     <h2>Get In Touch</h2>
                 </div>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repaatem.
+                Our team is excited to answer any questions you might have. We encourage you to reach out through our contact email or on any of our social media pages!
                 </p>
-            </div> 
+            </div>
             <div class="pure-u-1 vmid ex-st">
                     <div class="pure-u-1 pure-u-lg-1-2 pure-u-md-1-2 type-tile contact-info-tile">
                         <a href="mailto:contact@coincademy.com"><div class="type-tile-bg type-tile-express">
