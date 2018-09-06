ca1466f724466f12e2cbf358b32b6931d83cff8a
Alex Paden <ajp1016@sru.edu>
2018-09-05 20:20:54 -0400

    Content Update

--- wp-content/themes/coincademy_reborn/template-about-static.php
+++ wp-content/themes/coincademy_reborn/template-about-static.php
@@ -4,7 +4,7 @@
         <div class="pure-g vmid">
             <div class="pure-u-1 pure-u-md-1-1 pure-u-lg-1-1">
                 <blockquote>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Our goal is to provide an unparalleled financial education at zero cost to users worldwide.</p>
                     <p class="cite"> – The Coincademy Team</p>
                 </blockquote>
 
@@ -20,9 +20,9 @@
                 <h2>Our Story</h2>
             </div>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repaatem.
+            At Coincademy our goal has never been investmenting or profit, but rather working to usher in a new world financial system. It is through this philosophy that we decided to open an all inclusive academy for cryptocurrency and blockchain development. We believe that because this technology is so radically different than previous financial innovations we feel it necessary to educate the average user on why the technology is important without overwhelming or complicating.
             </p>
-        </div> 
+        </div>
         <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-1-2 vmid" data-relative-input="true" data-hover-only="true"  data-pointer-events="true" id="scene">
               <a style="margin-left: 60%; opacity: .7;" data-depth="0.2" href="/course-category/bitcoin/" >
                   <div class="category-tile vmid">
@@ -53,26 +53,26 @@
                     <h2>The Coincademy Team</h2>
                 </div>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repaatem.
+                As a team, Coincademy members have been actively engaged in cryptocurrency and blockchain technology since early 2011.
                 </p>
-            </div> 
+            </div>
         </div>
         <div class="pure-u-1 vmid ex-st">
             <div class="pure-u-1 pure-u-lg-1-3 pure-u-md-1-3 type-tile">
                 <div class="type-tile-bg  type-tile-standard">
-                    <span>Mark V.</span>
+                    <span>Mark Verhill</span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                 </div>
             </div>
             <div class="pure-u-1 pure-u-lg-1-3 pure-u-md-1-3 type-tile">
                 <div class="type-tile-bg  type-tile-standard">
-                    <span>Mark V.</span>
+                    <span>Alex Paden</span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                 </div>
             </div>
             <div class="pure-u-1 pure-u-lg-1-3 pure-u-md-1-3 type-tile">
                 <div class="type-tile-bg  type-tile-standard">
-                    <span>Mark V.</span>
+                    <span>Davis Smith</span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                 </div>
             </div>
